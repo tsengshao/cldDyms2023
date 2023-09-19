87ae056bf7ba017b67510bf97cb895c2175fe841
neko2048 <show.999991@gmail.com>
2023-09-19 15:15:32 +0800

    refactor(config): add fskao path

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 homePath = "./"
 vvmPath = "/data/mog/rce/"
 caseNameList = ["rce_walker_15k_1m_p3", "rce_walker_1k_1m_p3", "rce_walker_1k_2m_p3", "rce_walker_15k_05m_p3"]
+## Shao Path
 vvmPropertyPathShao = "/data/C.shaoyu/CD2023/dat/vvmProperties/"
 capecinPath = vvmPropertyPathShao + "capecin/"
+## Fskao Path
+vvmPropertyPathFskao = "/data/atmenu10246/cloudDyn/dat/vvmProperties/"
+msePath = vvmPropertyPathFskao + "mse/"
+streamFuncPath = vvmPropertyPathFskao + "streamFunc/"
 
 ## buoyancyPath = vvmPropertyPath + "buoyancy/"
 ## msePath = vvmPropertyPath + "mse/"
